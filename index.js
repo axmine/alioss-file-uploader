788b17340da5ee293d2be057553fa08f1b5e69b8
Rabbit <rabbit@RabbitdeiMac.local>
2020-06-23 00:07:28 +0800

    initUpdate

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * uploader class
+ * 1. create a uploader
+ * 2. vaildate the file's type, size, oth.
+ * 3. call ali-oss upload fn to upload files
+ */
+export class Uploader {
+  // start uploader and upload files
+  create (sts, opt = {}, callBack) {
+    console.log('create')
+  }
+}
